7965a798f8967aa25c22e626414c81bdd81dc7d8
GoEun Jeong <gogo8272@gmail.com>
2021-03-15 19:26:26 +0900

    [Modify] mobileprovision

--- PMS/SceneDelegate.swift
+++ PMS/SceneDelegate.swift
@@ -22,7 +22,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
-        // Create the SwiftUI view that provides the window contents.
         let contentView = StartView()
         let onlogin = LoginSettings()
         
